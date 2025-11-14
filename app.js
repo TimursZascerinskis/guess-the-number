b47e5e8c2c635e486aca945d1551317524864473
unknown <your@email.example>
2025-11-14 09:31:50 +0200

    finished proect

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-const number = 
+let secretNumber = Math.floor(Math.random() * 100) + 1;
+let attempts = 0;
+
+const input = document.getElementById('guessInput');
+const btn = document.getElementById('guessBtn');
+const message = document.getElementById('message');
+
+    btn.addEventListener('click', function () {
+        const value = input.value;
+        const guess = parseInt(value, 10);
+
+        if (isNaN(guess) || guess < 1 || guess > 100) {
+            message.textContent = "Please enter a valid number between 1 and 100.";
+        return;
+        }
+
+        attempts++;
+
+        if (guess === secretNumber) {
+            message.textContent = "Congratulations! You guessed the number in " + attempts + " attempts";
+
+        } else if (guess > secretNumber) {
+            message.textContent = "Too high! Try again.";
+        } else {
+            message.textContent = "Too low! Try ahain.";
+        }
+});
